b5a7cfbebdac36efc01b8de2029169dc61e9b4ad
AlexYu <yugai13912@gmail.com>
2021-02-22 19:03:40 +0600

    Version 1.1

--- src/main/java/com/company/syugai/json/serialization/GameSerialization.java
+++ src/main/java/com/company/syugai/json/serialization/GameSerialization.java
@@ -1,7 +1,6 @@
 package com.company.syugai.json.serialization;
 
 import com.company.syugai.model.Game;
-import com.company.syugai.model.User;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
